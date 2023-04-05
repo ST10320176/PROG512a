2cbc3c03f92d5c952f95bdea504a14e2bfb80263
ST10320176 <128587193+ST10320176@users.noreply.github.com>
2023-04-05 11:41:08 +0200

    Add files via upload

---
+++ examplemethod/src/main/java/com/mycompany/examplemethod/Examplemethod.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examplemethod;
+
+/**
+ *
+ * @author colin
+ */
+public class Examplemethod {
+
+    public static void main(String[] args) {
+        //display method
+        displayInfo();
+        
+        
+        System.out.println("Hello World!");
+    }
+    public static void displayInfo()     {
+        System.out.println("The weather is good today");
+    }
+}
